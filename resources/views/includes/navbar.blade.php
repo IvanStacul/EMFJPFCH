b58ba1d5f86612e940cd0805e5f3634578ff34bf
Ivan Stacul <stacul.ivan98@gmail.com>
2021-01-29 21:17:41 -0300

    imagenes para el jumbotron, logo del footer y del navbar agregado

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -10,6 +10,13 @@
           <span class="icon-bar"></span>
         </button>
 
+        <!-- LOGO -->
+        <div class="navbar-header">
+          <a class="navbar-brand" href=" {{ route('home') }} " aria-label="Logo">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo de EMFJPFCH" height="100%" />
+          </a>
+        </div>
+
         <!-- NAVBAR -->
         <div class="collapse navbar-collapse" id="main-navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
